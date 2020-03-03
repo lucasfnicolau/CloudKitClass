536dd49a30cea784dcace7e3110a327e1ab5bead
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-03-03 14:06:29 -0300

    Initial Commit

---
+++ CloudKitClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudKitClass
+//
+//  Created by Lucas Fernandez Nicolau on 03/03/20.
+//  Copyright © 2020 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
